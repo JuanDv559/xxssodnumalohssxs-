87a9be0057c9353ee04f1a746e55d7bc45fba105
JuanDv559 <deivid0411@hotmail.com>
2023-09-26 12:14:56 +0000

    antualizando nh

---
+++ server.js
@@ -0,0 +1,35 @@
+/*const exec = require('child_process').exec;
+const server = exec('http-server -c-1');
+server.stdout.on('data', function (data) {
+  if (data.includes('Available on')) {
+    //exec('open http://localhost:8080/index.html'); // Abre el archivo index.html en el navegador predeterminado (MacOS)
+     exec('start http://localhost:8080/index.html'); // Abre el archivo index.html en el navegador predeterminado (Windows)
+  }
+  process.stdout.write(data);
+});*/
+
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  const filePath = path.join(__dirname, 'index.html');
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(500);
+      res.end('Error en el servidor');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(content, 'utf-8');
+    }
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
+
+
