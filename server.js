048b60824a1f8388cd28104887fd10ca681a0113
david <deivid0411@hotmail.com>
2023-09-26 07:33:03 -0500

    actualizando repositorio cel

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const exec = require('child_process').exec;
+/*const exec = require('child_process').exec;
 const server = exec('http-server -c-1');
 server.stdout.on('data', function (data) {
   if (data.includes('Available on')) {
@@ -7,3 +7,28 @@ server.stdout.on('data', function (data) {
   }
   process.stdout.write(data);
 });
+*/
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  const filePath = path.join(__dirname, 'index.html');
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(500);
+      res.end('Error en el servidor');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(content, 'utf-8');
+    }
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
+
