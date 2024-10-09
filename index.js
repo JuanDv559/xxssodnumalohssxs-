e70e1a2aa9e74a966c67c99f859d087c229faa25
JuanDv559 <deivid0411@hotmail.com>
2024-10-09 15:57:29 -0500

    se empezo a usar js

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-			// Import stylesheets
+// Import stylesheets
 //import './style.css';
-
 
 // Write Javascript code!
 //const appDiv = document.getElementById('app');
 //appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-console.log("despierte david viasus");
+//console.log("despierte david viasus");
+class Usuario {
+    constructor(nombre, email) {}
+}
 
+let input = document.getElementById("nombre");
+
+let btnEnviar = document.getElementById("enviar");
+btnEnviar.addEventListener("click", () => {
+    console.log(input.value);
+});
